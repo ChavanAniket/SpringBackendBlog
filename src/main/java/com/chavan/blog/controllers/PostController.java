535513508cb4342e0025c765a14411bdffada628
ChavanAniket <aniketkc13062000@gmail.com>
2024-08-11 14:25:24 +0530

    vid 26 & 27 completed, Good Practice

--- src/main/java/com/chavan/blog/controllers/PostController.java
+++ src/main/java/com/chavan/blog/controllers/PostController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.chavan.blog.config.AppConstants;
 import com.chavan.blog.payload.ApiResponse;
 import com.chavan.blog.payload.PostDto;
 import com.chavan.blog.payload.PostResponse;
@@ -58,10 +59,10 @@ public class PostController {
 	// get all posts
 	@GetMapping("/posts")
 	public ResponseEntity<PostResponse> getAllPost(
-			@RequestParam(value="pageNumber", defaultValue ="0",required=false) Integer pageNumber,
-			@RequestParam(value="pageSize", defaultValue="5", required = false) Integer pageSize,
-			@RequestParam(value = "sortBy", defaultValue="postId", required = false) String sortBy,
-			@RequestParam(value = "sortDir", defaultValue="asc", required = false) String sortDir
+			@RequestParam(value="pageNumber", defaultValue =AppConstants.PAGE_NUMBER,required=false) Integer pageNumber,
+			@RequestParam(value="pageSize", defaultValue=AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+			@RequestParam(value = "sortBy", defaultValue=AppConstants.SORT_BY, required = false) String sortBy,
+			@RequestParam(value = "sortDir", defaultValue=AppConstants.SORT_DIR, required = false) String sortDir
 			){
 		PostResponse posts = this.postService.getAllPost(pageNumber, pageSize, sortBy,sortDir);
 		return new ResponseEntity<PostResponse>(posts,HttpStatus.OK);
